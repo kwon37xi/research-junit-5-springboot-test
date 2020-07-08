b46a4b11186ce46c742b53d2b33a213147f15272
KwonNam Son <kwon37xi@gmail.com>
2020-07-08 14:56:58 +0900

    initialize project

---
+++ src/main/java/kr/pe/kwonnam/researchjunit5springboottest/researchjunit5springboottest/ResearchJunit5SpringbootTestApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.kwonnam.researchjunit5springboottest.researchjunit5springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchJunit5SpringbootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchJunit5SpringbootTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/pe/kwonnam/researchjunit5springboottest/researchjunit5springboottest/ResearchJunit5SpringbootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.kwonnam.researchjunit5springboottest.researchjunit5springboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchJunit5SpringbootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
